47c76f8d6f78fa607943dbdb63f81b5b2d5a97b7
Jake Mingolla <jakemingolla@gmail.com>
2025-07-18 17:38:33 -0400

    checkpoint

--- src/tile-run.ts
+++ src/tile-run.ts
@@ -1,6 +1,15 @@
 import type { TileSet } from "./tile-set";
 import type { Tile } from "./tile";
 import { JokerTile, TileColor, NumberedTile } from "./tile";
+import { MissingTileError, InvalidJokerTileRemovalError } from "./tile-set";
+
+export class InvalidTileRemovalError extends Error {
+  constructor(tile: Tile) {
+    super(`Cannot remove tile ${tile} from a run.`);
+  }
+}
+
+const CANNOT_INSERT_SENTINEL = -1;
 
 export class TileRun implements TileSet {
   private tiles: Tile[];
@@ -38,6 +47,55 @@ export class TileRun implements TileSet {
     }
   }
 
+  private getInsertionIndex(tile: Tile): number {
+    let index = CANNOT_INSERT_SENTINEL;
+
+    if (this.tiles.length === 0) return 0;
+    if (this.tiles.every((t) => t instanceof JokerTile)) return 0;
+    if (tile instanceof JokerTile) return 0;
+
+    const tiles: Tile[] = this.tiles;
+    const firstTile = tiles.at(0)!;
+    const lastTile = tiles.at(-1)!;
+
+    if (
+      firstTile instanceof NumberedTile &&
+      tile.getNumber() === firstTile.getNumber() - 1
+    ) {
+      return 0;
+    }
+
+    if (
+      lastTile instanceof NumberedTile &&
+      tile.getNumber() === lastTile.getNumber() + 1
+    ) {
+      return tiles.length;
+    }
+
+    for (let i = 0; i < tiles.length; i++) {
+      console.log(`i: ${i}`);
+      const current = tiles.at(i)!;
+
+      if (!(current instanceof JokerTile)) {
+        console.log(`current is not a joker`);
+        continue;
+      }
+
+      const next = tiles.at(i + 1);
+      console.log(`current: ${current}, next: ${next}, tile: ${tile}, i: ${i}`);
+
+      if (next === undefined) {
+        return i;
+      } else if (next instanceof JokerTile) {
+        return i + 1;
+      } else if (tile.getNumber() + 1 === next.getNumber()) {
+        return i;
+      }
+    }
+
+    return index;
+  }
+
   isValid(): boolean {
     return this.tiles.length >= 3;
   }
@@ -51,39 +109,71 @@ export class TileRun implements TileSet {
       return false;
     }
 
-    const tiles = this.tiles;
+    const insertionIndex = this.getInsertionIndex(tile);
 
-    if (tiles.length === 0) {
+    if (insertionIndex === CANNOT_INSERT_SENTINEL) {
+      return false;
+    } else {
       return true;
     }
-
-    const firstTile: Tile = tiles.at(0)!;
-    const lastTile: Tile = tiles.at(-1)!;
-
-    if (firstTile instanceof JokerTile || lastTile instanceof JokerTile) {
-      return true;
-    }
-
-    if (tile.getNumber() === firstTile.getNumber() - 1) {
-      return true;
-    } else if (tile.getNumber() === lastTile.getNumber() + 1) {
-      return true;
-    }
-
-    // TODO check if joker can be replaced
-
-    return false;
   }
 
   add(tile: Tile): Tile | null {
-    return null;
+    if (tile instanceof JokerTile) {
+      this.tiles.push(tile);
+      return null;
+    }
+
+    const insertionIndex = this.getInsertionIndex(tile);
+    const existingTile = this.tiles.at(insertionIndex);
+
+    console.log(`insertionIndex: ${insertionIndex}`);
+    console.log(`existingTile: ${existingTile}`);
+
+    if (existingTile instanceof JokerTile) {
+      this.tiles[insertionIndex] = tile;
+      return existingTile;
+    } else {
+      this.tiles.splice(insertionIndex, 0, tile);
+      return null;
+    }
   }
 
   remove(tile: Tile): Tile {
-    return tile;
+    if (tile instanceof JokerTile) {
+      throw new InvalidJokerTileRemovalError();
+    }
+
+    const matchingIndex = this.tiles.findIndex((t) => t === tile);
+    if (matchingIndex === -1) {
+      throw new MissingTileError(tile);
+    }
+
+    if (matchingIndex === 0 || matchingIndex === this.tiles.length - 1) {
+      this.tiles.splice(matchingIndex, 1);
+      return tile;
+    } else {
+      throw new InvalidTileRemovalError(tile);
+    }
   }
 
   getSwapCandidates(): Tile[] {
-    return [];
+    if (this.tiles.length < 3) {
+      return [];
+    }
+
+    const firstTile = this.tiles.at(0)!;
+    const lastTile = this.tiles.at(-1)!;
+
+    const candidates: Tile[] = [];
+
+    if (!(firstTile instanceof JokerTile)) {
+      candidates.push(firstTile);
+    }
+    if (!(lastTile instanceof JokerTile)) {
+      candidates.push(lastTile);
+    }
+
+    return candidates;
   }
 }
--- test/unit/tile-run.test.ts
+++ test/unit/tile-run.test.ts
@@ -1,6 +1,10 @@
-import { TileRun } from "../../src/tile-run";
+import { TileRun, InvalidTileRemovalError } from "../../src/tile-run";
 import { describe, expect, test } from "bun:test";
 import { red7, red8, red9, red10, joker, black7 } from "../fixtures";
+import {
+  MissingTileError,
+  InvalidJokerTileRemovalError,
+} from "../../src/tile-set";
 
 describe("tile-run", () => {
   describe("validity", () => {
@@ -31,6 +35,108 @@ describe("tile-run", () => {
     test("cannot add a tile with a different color", () => {
       const run = new TileRun([red8, red9, red10]);
       expect(run.check(black7)).toBe(false);
+    });
+
+    test("cannot add a tile that does not match the run", () => {
+      const run = new TileRun([red7, red8, red9]);
+      expect(run.check(red7)).toBe(false);
+    });
+
+    test("cannot add a tile that does not match the run with a joker", () => {
+      const run = new TileRun([joker, red8, red9]);
+      expect(run.check(red8)).toBe(false);
+    });
+
+    test("can add a tile to replace a joker at the start of the run", () => {
+      const run = new TileRun([joker, red8, red9]);
+      expect(run.check(red7)).toBe(true);
+      expect(run.add(red7)).toBe(joker);
+      expect(run.isValid()).toBe(true);
+    });
+
+    test("can add a tile to replace a joker at the end of the run", () => {
+      const run = new TileRun([red7, red8, joker]);
+      expect(run.check(red9)).toBe(true);
+      expect(run.add(red9)).toBe(joker);
+      expect(run.isValid()).toBe(true);
+    });
+
+    test("can add a tile to replace a joker in the middle of the run", () => {
+      const run = new TileRun([red7, joker, red9]);
+      expect(run.check(red8)).toBe(true);
+      expect(run.add(red8)).toBe(joker);
+      expect(run.isValid()).toBe(true);
+    });
+
+    test("can add a numbered tile to an empty run", () => {
+      const run = new TileRun([]);
+      expect(run.check(red7)).toBe(true);
+      expect(run.add(red7)).toBe(null);
+      expect(run.check(red8)).toBe(true);
+      expect(run.check(red7)).toBe(false);
+      expect(run.check(black7)).toBe(false);
+    });
+
+    test("can add a joker to an empty run", () => {
+      const run = new TileRun([]);
+      expect(run.check(joker)).toBe(true);
+      expect(run.add(joker)).toBe(null);
+      expect(run.isValid()).toBe(false);
+    });
+
+    test("can add a numberered tile to a run with a single joker", () => {
+      const run = new TileRun([joker]);
+      expect(run.check(red7)).toBe(true);
+      expect(run.add(red7)).toBe(joker);
+      expect(run.isValid()).toBe(false);
+    });
+
+    test("can add a numbered tile to a run with a multiple jokers", () => {
+      const run = new TileRun([joker, joker]);
+      expect(run.check(red7)).toBe(true);
+      expect(run.add(red7)).toBe(joker);
+      expect(run.isValid()).toBe(false);
+    });
+  });
+
+  describe("removing tiles", () => {
+    test("can remove a tile from the run", () => {
+      const run = new TileRun([red7, red8, red9]);
+      expect(run.remove(red7)).toBe(red7);
+      expect(run.isValid()).toBe(false);
+    });
+
+    test("cannot remove a tile that is not in the run", () => {
+      const run = new TileRun([red7, red8, red9]);
+      expect(() => run.remove(red10)).toThrow(MissingTileError);
+    });
+
+    test("cannot remove a tile that is not the first or last tile", () => {
+      const run = new TileRun([red7, red8, red9]);
+      expect(() => run.remove(red8)).toThrow(InvalidTileRemovalError);
+    });
+
+    test("cannot remove a joker tile", () => {
+      expect(() => new TileRun([]).remove(joker)).toThrow(
+        InvalidJokerTileRemovalError,
+      );
+    });
+  });
+
+  describe("getting swap candidates", () => {
+    test("returns an empty array if the run is less than 3 tiles", () => {
+      const run = new TileRun([red7]);
+      expect(run.getSwapCandidates()).toEqual([]);
+    });
+
+    test("returns the first and last tiles if they are not jokers", () => {
+      const run = new TileRun([red7, red8, red9]);
+      expect(run.getSwapCandidates()).toEqual([red7, red9]);
+    });
+
+    test("does not return jokers if they are the first or last tile", () => {
+      const run = new TileRun([joker, red7, red8, red9, joker]);
+      expect(run.getSwapCandidates()).toEqual([]);
     });
   });
 });
